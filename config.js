57bd73fcfbb7a88250307e3146415227634c578a
Ffhihi <106500772+Ffhihi@users.noreply.github.com>
2022-05-30 09:35:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Bạn muốn ăn gì",
+    introTitle: 'Vũ ngọc Lân',
+    introDesc: `Bạn muốn ăn gì nào bạn muốn ăn 
+    một tô mì hông `,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Bạn ăn cứt nhá oki hông 🥰',
+    desc: 'Hay bạn thích ăn đấm nhỉ ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới có rất nhiều cái ăn sao bạn lại chọn cứt <3',
+    btnReply: 'Gửi cho Đức đẹp zai <3',
+    reply: 'tao muốn ăn cứt <33333333',
+    mess: 'Mình biết mà 🥰. thằng ngu 😘😘',
+    messDesc: 'Tối nay , nấu cơm cho mình nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
